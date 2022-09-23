d5ff313e7bbc796b94d39d6f3e12b90cec1694a6
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-23 14:35:36 +0700

    feat: type and brand controllers

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express');
 const router = new Router();
+const brandController = require('../controllers/brandController');
 
-router.post('/add',);
+router.post('/add', brandController.create);
 router.delete('/delete',);
 router.get('/get',);
+router.get('/all', brandController.getAll);
 router.post('/update',);
 
 module.exports = router;
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express');
 const router = new Router();
+const typeController = require('../controllers/typeController')
 
-router.post('/add',);
+router.post('/add', typeController.create);
 router.delete('/delete',);
 router.get('/get',);
+router.get('/all', typeController.getAll);
 router.post('/update',);
 
 module.exports = router;
