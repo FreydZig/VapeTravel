7b2bbcec01b532db79844c487f110c2d4d972ce7
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-27 14:13:53 +0700

    feat: ProductsStore

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserStore from "./store/UserStore";
+import ProductsStore from "./store/ProductsStore";
 
 export const Context = createContext(null);
 
@@ -9,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Context.Provider value={{
-          user: new UserStore()
+          user: new UserStore(),
+          products: new ProductsStore()
       }}>
           <App />
       </Context.Provider>
---
+++ client/src/store/ProductsStore.js
@@ -0,0 +1,43 @@
+import {makeAutoObservable} from "mobx";
+
+export default class ProductsStore{
+    constructor() {
+        this._types = [
+            {id: 1, name: 'Name 1'},
+            {id: 2, name: 'Name 2'}
+        ];
+        this._brands = [
+            {id: 1, name: 'BName 1'},
+            {id: 2, name: 'BName 2'}
+        ];
+        this._products = [
+            {id: 1, name: 'BName 1', description: 'fasfsa', price: 2223, img: 'https://img2.goodfon.ru/original/3694x2463/6/9a/evropeyskaya-koshka-dikiy-kot.jpg'},
+            {id: 2, name: 'pName 2', description: 'fasfsa', price: 2223, img: 'https://wallpapersgood.ru/wallpapers/main/201128/koshka-kot-polosatyy-b0b9c75.jpg'}
+        ];
+        makeAutoObservable(this);
+    }
+
+    setTypes(types){
+        this._types = types;
+    }
+
+    setBrands(brands){
+        this._brands = brands;
+    }
+
+    setProducts(products){
+        this._products = products;
+    }
+
+    get types(){
+        return this._types;
+    }
+
+    get brands(){
+        return this._brands;
+    }
+
+    get products(){
+        return this._products;
+    }
+}
